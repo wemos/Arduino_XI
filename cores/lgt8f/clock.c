7bf1d96fc9c89abe67c445fb0dec201055b64e57
wemos <support@wemos.cc>
2016-10-26 09:09:59 +0800

    fix clock.c

--- cores/lgt8f/clock.c
+++ cores/lgt8f/clock.c
@@ -4,7 +4,11 @@
 
 #include "Arduino.h"
 
-
+/*
+clk: 
+	1 - switch to external crystal
+	0 - switch to inside RC CLock
+*/
 void sysClock(uint8_t clk)
 {
 	volatile unsigned char btmp = 0;
@@ -21,11 +25,24 @@ void sysClock(uint8_t clk)
 		PMCR = 0x80;
 		PMCR = btmp;
 
+		// 1 system clock
 		CLKPR = 0x80;
 		CLKPR = 0x00;
 	}
 	else
 	{
+		// 1/2 system clock
+		CLKPR = 0x80;
+		CLKPR = 0x01;
+
 		//switch to inside RC CLock
+		btmp = PMCR & 0x1f;   //0001 1111
+		PMCR = 0x80;
+		PMCR = btmp;
+
+		// CLose the external crystal clock
+		btmp = PMCR & 0x7B;   //0111 1011
+		PMCR = 0x80;
+		PMCR = btmp;
 	}
 }
