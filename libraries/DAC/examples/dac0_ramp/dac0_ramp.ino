40af6f871aec97980ed9b0f29acb344e2b1ed57d
wemos <support@wemos.cc>
2018-06-27 16:12:43 +0800

    fix

--- libraries/DAC/examples/dac0_ramp/dac0_ramp.ino
+++ libraries/DAC/examples/dac0_ramp/dac0_ramp.ino
@@ -1,6 +1,5 @@
 //============================================
 // DAC0 output demo 
-// DACO output ==> D4 on board
 //============================================
 
 unsigned char value = 0;
@@ -8,14 +7,14 @@ unsigned char value = 0;
 void setup() {
   // put your setup code here, to run once:
   analogReference(INTERNAL2V56);
-  pinMode(4, ANALOG);
+  pinMode(DAC0, ANALOG);
 
-  analogWrite(4, 255);
+  analogWrite(DAC0, 255);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //analogWrite(4, value);
+  analogWrite(DAC0, value);
   delay(10);
   value += 10;
 }
